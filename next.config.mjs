8c66ca43bc86db9ba6d0993ae2c63718253d2c9a
FastBack2499 <Roham.Esmaili1379@gmail.com>
2025-03-17 02:35:22 +0330

    modified next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,10 @@
 
 const nextConfig = {
   output: 'export',
-  basePath: '/misagh17.github.io',
-  assetPrefix: '/misagh17.github.io/',
   trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
