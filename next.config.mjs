6552bc3438d2af9e6e309ecca5290552f5ac0846
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-03-17 13:30:52 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  output: 'export',
-  basePath: '/misagh17.github.io',
-  assetPrefix: '/misagh17.github.io/',
-  trailingSlash: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
