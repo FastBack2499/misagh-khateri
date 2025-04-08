005d636b91dea42ffb8d5b2bd3b39970ac0b8998
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-04-08 21:08:40 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Misagh Khateri",
   description: "Misagh Khateri Financial Advisor",
+  alternates: {
+    canonical: "https://www.misagh-khateri.info"
+  }
 };
 
 export default function RootLayout({
