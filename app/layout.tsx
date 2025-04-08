8249e11faa2f5f721fd0a0e54b44cecb709b3007
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-04-08 22:02:29 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,14 @@ import usePathname from "@/app/lib/usePathname";
 const pathname = usePathname
 
 export const metadata: Metadata = {
-  title: "Misagh Khateri",
-  description: "Misagh Khateri Financial Advisor",
+  title: "Misagh Khateri – Experienced Financial Adviser Specializing in Banking and Market Analysis",
+  description: "Explore the professional portfolio of Misagh Khateri, a San Francisco-based financial adviser with over a decade of experience in banking, trading, and financial market analysis. Discover services ranging from SWIFT transactions to client portfolio management.",
   alternates: {
     canonical: `https://www.misagh-khateri.info${pathname}`
-  }
+  },
+  keywords: [
+    "Financial Adviser", "Banking Services", "Market Analysis", "SWIFT Transactions", "Client Portfolio Management", "Financial Consulting", "San Francisco Finance Professional", "Investment Strategies", "Budgeting and Forecasting", "Wealth Management"
+  ]
 };
 
 export default function RootLayout({
