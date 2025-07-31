b12baa5cd3d552cb5f01efd5845d949b1df4313e
Roham Esmaeili <64218658+FastBack2499@users.noreply.github.com>
2025-07-31 18:55:51 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   title: "Misagh Khateri – Experienced Financial Adviser Specializing in Banking and Market Analysis",
   description: "Explore the professional portfolio of Misagh Khateri, a San Francisco-based financial adviser with over a decade of experience in banking, trading, and financial market analysis. Discover services ranging from SWIFT transactions to client portfolio management.",
   alternates: {
-    canonical: `https://www.misagh-khateri.info/`
+    canonical: `https://www.misagh-khateri.info/${pathname}`
   },
   keywords: [
     "Financial Adviser", "Banking Services", "Market Analysis", "SWIFT Transactions", "Client Portfolio Management", "Financial Consulting", "San Francisco Finance Professional", "Investment Strategies", "Budgeting and Forecasting", "Wealth Management"
